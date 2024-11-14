05a62eb355808425567eebf5a9c6047b1aad821f
jackreiman9 <jackreiman9@gmail.com>
2024-11-14 13:38:30 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EcoPulseQuiz from './components/EcoPulse/EcoPulseQuiz';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <EcoPulseQuiz />
+    </div>
+  );
+}
+
+export default App;
